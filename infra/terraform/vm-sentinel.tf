aecb6723e1fe2ee0804ed7447484e335b56b8701
Daniel Cruz <sp_dicruz044@hotmail.com>
2026-01-12 18:08:06 -0500

    Refactor AWS EC2 instance and key pair configuration

--- infra/terraform/vm-sentinel.tf
+++ infra/terraform/vm-sentinel.tf
@@ -25,3 +25,23 @@ resource "aws_instance" "sentinel" {
   })
 
 }
+
+data "aws_ami" "ubuntu_22_04" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
